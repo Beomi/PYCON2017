308f7722a8b03937f6b489eb2a5d7b9871efc1d0
Beomi <latheledusjp@gmail.com>
2017-08-05 00:30:46 +0900

    fix typo

--- session/onoffmix.py
+++ session/onoffmix.py
@@ -14,11 +14,11 @@ def main():
             'pw': 'mypassword1234',
             'proc': 'login'
         })
-        # get Datas
+        # 신청 이벤트 페이지 가져오기
         html = s.get('http://onoffmix.com/account/event')
         soup = bs(html.text, 'html.parser')
 
-        # 신청 이벤트 콕록 만들기
+        # 신청 이벤트 목록 만들기
         event_list = soup.select('#eventListHolder > div > ul > li.title > a')
         for event in event_list:
             print(event.text)
