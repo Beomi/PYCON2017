d4c583055eed0d46287b535182b8b1d3f2d5d7ff
Beomi <latheledusjp@gmail.com>
2017-08-07 22:12:56 +0900

    revert to static name

--- session/facebook_headless.py
+++ session/facebook_headless.py
@@ -1,22 +1,19 @@
 from selenium import webdriver
-from datetime import datetime
 
 options = webdriver.ChromeOptions()
 options.add_argument('headless')
 options.add_argument('window-size=1920x1080')
 
 driver = webdriver.Chrome('chromedriver', chrome_options=options)
-
 driver.get('https://facebook.com')
 driver.implicitly_wait(3)
 
 email = driver.find_element_by_css_selector('input[type=email]')
 password = driver.find_element_by_css_selector('input[type=password]')
 login = driver.find_element_by_css_selector('input[type="submit"]')
-
 email.send_keys('username@mail.com')
 password.send_keys('ilovepython')
 login.click()
 
-driver.get_screenshot_as_file('{}.png'.format(datetime.now()))
+driver.get_screenshot_as_file('facebook.png')
 driver.quit()
