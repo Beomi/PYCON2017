30827d6e587109837e1bd4ec596c262486d0de1a
beomi <latheledusjp@gmail.com>
2017-08-13 10:04:59 +0900

    get a links instead of whole text

--- lightening/parser.py
+++ lightening/parser.py
@@ -1,21 +1,19 @@
 import requests
 import telegram
 import time
+from bs4 import BeautifulSoup as bs
 
 bot = telegram.Bot(token='')
 chat_id = bot.getUpdates()[-1].message.chat.id
 
+links = []
+
 while True:
-    f = open('html.html', 'w+')
-
     r = requests.get('https://www.pycon.kr/2017/program/tutorials/')
-
-    if r.text != f.read():
-        bot.sendMessage(chat_id=chat_id, text='파이콘 사이트가 뭔가 바뀌었어요! https://www.pycon.kr/2017/program/tutorials/')
-
-    f.write(r.text)
-
-    f.close()
+    soup = bs(r.text, 'html.parser')
+    new_link = soup.select('a')
+    if new_link != links:
+        bot.sendMessage(chat_id=chat_id, text='https://www.pycon.kr/2017/program/tutorials/')
+        links = new_link
 
     time.sleep(30)
-
